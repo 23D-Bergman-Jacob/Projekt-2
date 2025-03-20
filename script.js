f97816f0a42d36a28546377219a426f3cb68cf84
Jacob Bergman <Jacob.Bergman@elev.ga.ntig.se>
2025-03-20 13:16:49 +0100

    Javascript för att lägga till saker i lista nu färdigt

---
+++ script.js
@@ -0,0 +1,49 @@
+let varukorgValue = 0
+const varukorg_items = []
+let prise
+let varubild = []
+
+function vitTröja()
+{
+    varukorgValue++
+    varukorg_items.push('vitTröja')
+    varubild.push("img/vitw.webp")
+    console.log("hejsan");   
+    prise = varukorg_items.length*499
+}
+
+function beigeTröja()
+{
+    varukorgValue++
+    varukorg_items.push('beigeTröja')
+    prise = varukorg_items.length*499
+}
+
+function ljusbrunTröja()
+{
+    varukorgValue++
+    varukorg_items.push('ljusbrunTröja')
+    prise = varukorg_items.length*499
+    console.log("hejsan");   
+}
+
+function mörkgråTröja()
+{
+    varukorgValue++
+    varukorg_items.push('mörkgråTröja')
+    prise = varukorg_items.length*499
+}
+
+function mörkgrönTröja()
+{
+    varukorgValue++
+    varukorg_items.push('mörkgrönTröja')
+    prise = varukorg_items.length*499   
+}
+
+function turkosTröja()
+{
+    varukorgValue++
+    varukorg_items.push('turkosTröja')
+    prise = varukorg_items.length*499
+}
